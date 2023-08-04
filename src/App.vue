29f789615fd94dc8f098b8002373fa5ceae9ca80
Neilly28 <neilpilarca@gmail.com>
2023-08-04 23:21:48 +0200

    added background image

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <HeaderView />
-  <router-view />
+  <div
+    class="bg-[url('../public/background.png')] bg-repeat-space bg-right-bottom"
+  >
+    <HeaderView />
+    <router-view />
+  </div>
 </template>
 
 <script setup>
 import HeaderView from "./components/HeaderView.vue";
 </script>
-
-<style></style>
